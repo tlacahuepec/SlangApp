1a4a8c23cee44e813d576561652941524b243e02
Edward Santiesteban <Edward.Santiesteban@aa.com>
2020-08-08 14:02:00 -0500

    Added progress bar

--- app/src/androidTest/java/com/aa/slangapp/MainActivityInstrumentationTest.kt
+++ app/src/androidTest/java/com/aa/slangapp/MainActivityInstrumentationTest.kt
@@ -1,13 +1,17 @@
 package com.aa.slangapp
 
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.closeSoftKeyboard
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import org.hamcrest.core.IsNot.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -30,6 +34,18 @@ class MainActivityInstrumentedTest {
         onView(withId(R.id.buttonSearch))
             .check(matches(withText(BUTTON_STRING)))
             .perform(click())
+    }
+
+    @Test
+    fun shouldShowProgressBarAfterSearchButtonClick() {
+        onView(withId(R.id.progressBar))
+            .check(matches(not(isDisplayed())))
+        onView(withId(R.id.buttonSearch))
+            .check(matches(withText(BUTTON_STRING)))
+            .perform(click())
+        // TODO: assert after view model addition
+        // onView(withId(R.id.progressBar))
+        //   .check(matches(isDisplayed()))
     }
 
     companion object {
