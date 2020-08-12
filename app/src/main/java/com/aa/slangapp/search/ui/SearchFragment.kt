25a637bd75ca9cda548d9470bb23899e43e297e3
Edward Santiesteban <Edward.Santiesteban@aa.com>
2020-08-12 14:47:16 -0500

    removed unused view model

--- app/src/main/java/com/aa/slangapp/search/ui/SearchFragment.kt
+++ app/src/main/java/com/aa/slangapp/search/ui/SearchFragment.kt
@@ -5,20 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import com.aa.slangapp.com.aa.slangapp.dependencyInjection.Injectable
-import com.aa.slangapp.com.aa.slangapp.dependencyInjection.injectViewModel
 import com.aa.slangapp.com.aa.slangapp.search.ui.hideKeyboard
 import com.aa.slangapp.databinding.FragmentSearchBinding
-import javax.inject.Inject
 
 
-class SearchFragment : Fragment(), Injectable {
-
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
-    private lateinit var viewModel: SearchViewModel
+class SearchFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,14 +18,7 @@ class SearchFragment : Fragment(), Injectable {
         savedInstanceState: Bundle?
     ): View? {
 
-        viewModel = injectViewModel(viewModelFactory)
-
         val binding = FragmentSearchBinding.inflate(inflater, container, false)
-
-        // Set the LifecycleOwner to be able to observe LiveData objects
-        binding.lifecycleOwner = this
-        // Bind ViewModel
-        binding.viewmodel = viewModel
 
         binding.buttonSearch.setOnClickListener {
             hideKeyboard()
--- app/src/main/java/com/aa/slangapp/search/ui/SearchViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.aa.slangapp.search.ui
-
-import androidx.lifecycle.ViewModel
-import javax.inject.Inject
-
-class SearchViewModel @Inject constructor() : ViewModel() {
-
-}
