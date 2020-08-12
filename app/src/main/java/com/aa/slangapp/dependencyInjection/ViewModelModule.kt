04e71f49a97d158d014e1b261e10ffab913a61ef
Jaime Santiesteban <tlacahuepec@users.noreply.github.com>
2020-08-12 14:48:11 -0500

    removed unused view model (#13)
    Co-authored-by: Edward Santiesteban <Edward.Santiesteban@aa.com>

--- app/src/main/java/com/aa/slangapp/dependencyInjection/ViewModelModule.kt
+++ app/src/main/java/com/aa/slangapp/dependencyInjection/ViewModelModule.kt
@@ -3,17 +3,12 @@ package com.aa.slangapp.com.aa.slangapp.dependencyInjection
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.aa.slangapp.search.ui.SearchResultsViewModel
-import com.aa.slangapp.search.ui.SearchViewModel
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
 @Module
 abstract class ViewModelModule {
-    @Binds
-    @IntoMap
-    @ViewModelKey(SearchViewModel::class)
-    abstract fun bindSearchViewModel(viewModel: SearchViewModel): ViewModel
 
     @Binds
     @IntoMap
